14f53b3fbf8c22a22f86a7ea236310edd56f7509
unknown <wdq0223@gmail.com>
2024-08-22 01:17:43 +0800

    fix the model to v10

--- distance.py
+++ distance.py
@@ -1,9 +1,11 @@
-# names: {0: 'bicycle', 1: 'car', 2: 'cat', 3: 'cone', 4: 'crosswalk', 5: 'dog', 6: 'scooter', 7: 'motorcycle', 8: 'people', 9: 'stairs', 10: 'red-light', 11: 'yellow-light', 12: 'green-light'}
+# names: {0: 'bicycle', 1: 'car', 2: 'cat', 3: 'cone', 4: 'crosswalk', 5: 'dog', 6: 'scooter', 7: 'motorcycle',
+# 8: 'people', 9: 'stairs', 10: 'red-light', 11: 'yellow-light', 12: 'green-light'}
 
 foc = 2660
 real_height_car = 1.57
 real_height_person = 1.70
 real_height_motorcycle = 1.30
+real_height_list = [1, 1.57, 0.5, 1.2, 0, 0.7, 1.35, 1.3, 1.7, 0, 0, 0, 0]
 
 def distance(results):
     boxes = results.boxes
@@ -18,6 +20,7 @@ def distance(results):
         for j, name in enumerate(names):
             if cls[i] == name:
                 print(f'Name:{names[j]}')
+                distance.append((foc * real_height_list[j]) / h)
 
 
 
--- yolov10-detect.py
+++ yolov10-detect.py
@@ -1,19 +1,18 @@
 import cv2
 import supervision as sv
-from ultralytics import YOLO
+from ultralytics import YOLOv10
 import distance
 
-model = YOLO(f'best51.pt')
+model = YOLOv10(f'trained_weight/best.pt')
 bounding_box_annotator = sv.BoundingBoxAnnotator()
-label_annotator = sv.LabelAnnotator()  
-cap=cv2.VideoCapture('test5.mp4')
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+label_annotator = sv.LabelAnnotator()
+cap=cv2.VideoCapture('video/V_20240821_125044_ES5.mp4')
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 if not cap.isOpened():
     print("can't open the webcam")
 while True:
     ret,frame=cap.read()
-    # frame = cv2.resize(frame, (128, 72), interpolation=cv2.INTER_AREA)
     if not ret:
         break
     results = model(frame)[0]
@@ -30,7 +29,7 @@ while True:
         cv2.putText(annotated_image, label_text, (int(x) - int(y) - 10), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)"""
     annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
-    annotated_image = cv2.resize(annotated_image, (128*5, 72*5), interpolation=cv2.INTER_AREA)
+    annotated_image = cv2.resize(annotated_image, (192*5, 108*5), interpolation=cv2.INTER_AREA)
     cv2.imshow('Webcam',annotated_image)
     k=cv2.waitKey(1)
     if k%256==27:
