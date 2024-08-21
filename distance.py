3ffb87d6b02d16a6ade8c3b5908707fd4db55057
Joey Wang <joey.wang@asml.com>
2024-08-21 16:45:31 +0800

    modify

--- distance.py
+++ distance.py
@@ -1,13 +1,13 @@
-# names: {0: 'bike', 1: 'car', 2: 'cat', 3: 'dog',
-#            4: 'motorcycle', 5: 'stairs', 6: 'green-light', 
-#               7: 'red-light', 8: 'yellow-light'}
+# names: {0: 'bicycle', 1: 'car', 2: 'cat', 3: 'cone', 4: 'crosswalk', 5: 'dog', 6: 'scooter', 7: 'motorcycle', 8: 'people', 9: 'stairs', 10: 'red-light', 11: 'yellow-light', 12: 'green-light'}
 
 foc = 2660
 real_height_car = 1.57
 real_height_person = 1.70
 real_height_motorcycle = 1.30
 
-def distance(boxes):
+def distance(results):
+    boxes = results.boxes
+    names = results.names
     distance = []
     cls = boxes.cls.tolist()
     cls = [clss for clss in cls]
@@ -15,27 +15,10 @@ def distance(boxes):
 
     for i, coords in enumerate(boxes.xywh.tolist()):
         h = coords[3]
-        if cls[i] == 0:
-            print("bike")
-        if cls[i] == 1:
-            print("car")
-            distance.append((foc * real_height_car)/h)
-        if cls[i] == 2:
-            print("cat")
-        if cls[i] == 3:
-            print("dog")
-        if cls[i] == 4:
-            print("motorcycle")
-            distance.append((foc * real_height_motorcycle)/h)
-        if cls[i] == 5:
-            print("stairs")
-            distance.append((foc * real_height_person)/h)
-        if cls[i] == 6:
-            print("green-light")
-        if cls[i] == 7:
-            print("red-light")
-        if cls[i] == 8:
-            print("yellow-light")
+        for j, name in enumerate(names):
+            if cls[i] == name:
+                print(f'Name:{names[j]}')
+
 
 
 
--- yolov10-detect.py
+++ yolov10-detect.py
@@ -3,10 +3,10 @@ import supervision as sv
 from ultralytics import YOLO
 import distance
 
-model = YOLO(f'best45.pt')
+model = YOLO(f'best51.pt')
 bounding_box_annotator = sv.BoundingBoxAnnotator()
 label_annotator = sv.LabelAnnotator()  
-cap=cv2.VideoCapture('test2.mp4')
+cap=cv2.VideoCapture('test5.mp4')
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 if not cap.isOpened():
@@ -17,14 +17,18 @@ while True:
     if not ret:
         break
     results = model(frame)[0]
-    # print(results)
     boxes = len(results.boxes.xywh.tolist())
     if boxes != 0:
-        dist = distance.distance(boxes=results.boxes)
-        print(dist)
+        dist = distance.distance(results=results)
+        print(f'Distance:{dist}')
     detections = sv.Detections.from_ultralytics(results)
-    print(detections)
+    # print(detections)
     annotated_image = bounding_box_annotator.annotate(scene=frame, detections=detections)
+    """for i, box in enumerate(results.boxes.xywh.tolist()):
+        x, y, w, h = box
+        label_text = f"Distance:{dict[i]:.2f}" if dict else "NO distance"
+        cv2.putText(annotated_image, label_text, (int(x) - int(y) - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 255, 0), 2)"""
     annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
     annotated_image = cv2.resize(annotated_image, (128*5, 72*5), interpolation=cv2.INTER_AREA)
     cv2.imshow('Webcam',annotated_image)
